c179893bfcf05d2218333b184d4621bfa435ad97
Eugene <63777417+nadProg@users.noreply.github.com>
2021-03-21 12:40:02 +0700

    Add deploy config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ?
+    '/site-constructor/' :
+    '/',
   chainWebpack: (config) => {
     const svgRule = config.module.rule('svg');
 
