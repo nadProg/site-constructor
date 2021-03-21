db7611bdde647e4c988b2ad9ff35283c1f2a6bf2
Eugene <63777417+nadProg@users.noreply.github.com>
2021-03-21 15:07:04 +0700

    Patch v0.1.1
    - Change project name

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ?
-    '/site-constructor/' :
+    '/vue-site-constructor/' :
     '/',
   chainWebpack: (config) => {
     const svgRule = config.module.rule('svg');
